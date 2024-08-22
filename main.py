4cdb00d723609f8dc86a12424a0c266b94fbb943
Arvin <noelarvin96@gmail.com>
2024-08-22 20:24:21 +0530

    Updated url of Angular

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routes.routes import routes_bp  # Import the Blueprint
 app = Flask(__name__)
 
 # Enable CORS for all routes, specifying origins if needed
-CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
+CORS(app, resources={r"/*": {"origins": "https://angular-data-visual-render.onrender.com"}})
 
 # Register the Blueprint
 app.register_blueprint(routes_bp)
