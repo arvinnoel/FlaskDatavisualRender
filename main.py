9d3a9de0cd1eb07f6689498330b906da2699466d
Arvin <noelarvin96@gmail.com>
2024-08-22 15:47:39 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.routes import routes_bp  # Import the Blueprint
-from waitress import serve
 
 app = Flask(__name__)
 
@@ -10,7 +9,3 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
 
 # Register the Blueprint
 app.register_blueprint(routes_bp)
-
-if __name__ == '__main__':
-    # Bind to 0.0.0.0 to make the app accessible externally
-    serve(app, host='0.0.0.0', port=8080)
